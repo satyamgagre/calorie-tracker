a54749d6a51d36637697df9c7a23265a71bed9ef
Satyam Gagare <satyamgagre4@gmail.com>
2025-06-21 23:49:59 +0530

    ,

--- fitcore/tracker/models.py
+++ fitcore/tracker/models.py
@@ -1,13 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
 class Food(models.Model):
+    name = models.CharField(max_length=100)
+    carbs = models.FloatField(help_text="in grams")
+    protein = models.FloatField(help_text="in grams")
+    fats = models.FloatField(help_text="in grams")
+    calories = models.IntegerField(help_text="in kcal")
 
     def __str__(self):
         return self.name
 
-    name = models.CharField(max_length=100)
-    carbs = models.FloatField()
-    protein = models.FloatField()
-    fats = models.FloatField()
-    calories = models.IntegerField()
+class Consume(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    food_consume = models.ForeignKey(Food, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} consumed {self.food_consume.name}"
